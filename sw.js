02733d48fae532ffcc72d5a0414872be580f7ff6
IvanDanyliuk <itsjustforserfing@gmail.com>
2021-06-26 20:22:24 +0300

    added dynamic caching

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
 const staticCacheName = 's-app-v3';
+const dynamicCacheName = 'd-app-v3';
 const assetsUrls = [
     'index.html',
     'js/index.js',
-    'css/style.css'
+    'css/style.css',
+    'offline.html'
 ]
 
 self.addEventListener('install', async event => {
@@ -15,16 +17,36 @@ self.addEventListener('activate', async event => {
     await Promise.all(
         cacheNames
             .filter(name => name !== staticCacheName)
+            .filter(name => name !== dynamicCacheName)
             .map(name => caches.delete(name))
     )
 });
 
 self.addEventListener('fetch', event => {
-    console.log('Fetch', event.request.url);
-    event.respondWith(cacheFirst(event.request));
+    const {request} = event;
+    const url = new URL(request.url);
+
+    if(url.origin === location.origin) {
+        event.respondWith(cacheFirst(request));
+    } else {
+        event.respondWith(networkFirst(request));
+    }    
 });
 
-const cacheFirst = async (request) => {
+const cacheFirst = async request => {
     const cashedData = await caches.match(request);
-    return cashedData ?? fetch(request);
+    return cashedData ?? await fetch(request);
+};
+
+const networkFirst = async request => {
+    const cacheData = await caches.open(dynamicCacheName);
+    try {
+        const response = await fetch(request);
+        await cacheData.put(request, response.clone());
+        return response;
+    } catch (error) {
+        const cached = await cacheData.match(request);
+        return cached ?? await caches.match('/offline.html');
+    }
+    
 }
